420b13a31262fea79bbe1f38ca76f8248bd81f3d
danielforeverlearning <danielforeverlearning@gmail.com>
2019-09-09 10:58:57 -1000

    Add files via upload

---
+++ oracle_db_test1.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+//(0) C:\ora11gr2\product\11.2.0\client_1\network\admin\tnsnames.ora
+//(1) download ojdbc14.jar
+//(2) paste into jre/lib/ext folder
+//(3) set classpath=C:\Program Files\Java\jdk1.8.0_144\jre\lib\ext\ojdbc14.jar;.;
+
+
+class OracleCon {
+	
+	public static void main(String args[]) {
+	
+		try {
+			
+			//step1 load the driver class
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//step2 create the connection object
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@myhost:1521:myservice","username","password");
+			
+			//step3 create the statement object
+			Statement stmt = con.createStatement();
+			
+			//step4 execute query
+			ResultSet rs   = stmt.executeQuery("SELECT STR1, STR2 FROM MYTABLE");
+			
+			while(rs.next())
+				System.out.println(rs.getString(1) + "   " + rs.getString(2));
+			
+			//step5 close the connection object
+			con.close();
+		}
+		
+		catch (Exception ee) {
+			System.out.println(ee);
+		}
+	
+	}
+	
+}
